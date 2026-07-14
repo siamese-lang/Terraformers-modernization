992854798b0628ea0b45b24cffb4ece4da58dca3
siamese-lang <askj95@naver.com>
2026-07-14 21:39:43 +0900

    Restore testable runtime config inspector constructor

--- backend/src/main/java/com/terraformers/modernization/config/RuntimeConfigInspector.java
+++ backend/src/main/java/com/terraformers/modernization/config/RuntimeConfigInspector.java
@@ -1,6 +1,7 @@
 package com.terraformers.modernization.config;
 
 import java.util.List;
+import java.util.function.Function;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,15 +9,21 @@ import org.springframework.stereotype.Service;
 public class RuntimeConfigInspector {
 
     private final RuntimeContractProperties properties;
+    private final Function<String, String> envSupplier;
 
     @Autowired
     public RuntimeConfigInspector(RuntimeContractProperties properties) {
+        this(properties, System::getenv);
+    }
+
+    RuntimeConfigInspector(RuntimeContractProperties properties, Function<String, String> envSupplier) {
         this.properties = properties;
+        this.envSupplier = envSupplier;
     }
 
     public List<RuntimeConfigStatus> inspectRequiredEnv() {
         return properties.getRequiredEnv().stream()
-                .map(name -> new RuntimeConfigStatus(name, hasText(System.getenv(name))))
+                .map(name -> new RuntimeConfigStatus(name, hasText(envSupplier.apply(name))))
                 .toList();
     }
 
