760c4d420e5fe4f50c602be059272f3b3e588c44
siamese-lang <askj95@naver.com>
2026-07-14 19:08:15 +0900

    Add web package info

---
+++ backend/src/main/java/com/terraformers/modernization/web/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Internal web endpoints used for runtime readiness checks.
+ *
+ * <p>Endpoints in this package are operational check surfaces and should be
+ * protected from public exposure in real deployments.</p>
+ */
+package com.terraformers.modernization.web;
