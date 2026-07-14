1bdb1a1068f7752d8c51155c09aba85b8278e3ac
siamese-lang <askj95@naver.com>
2026-07-14 23:25:09 +0900

    Import frontend event bus utility

---
+++ frontend/src/utils/eventBus.js
@@ -0,0 +1,32 @@
+class EventBus {
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(event, callback) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push(callback);
+  }
+
+  emit(event, data) {
+    if (this.listeners[event]) {
+      this.listeners[event].forEach((callback) => {
+        try {
+          callback(data);
+        } catch (error) {
+          console.error(`Error in event listener for ${event}:`, error);
+        }
+      });
+    }
+  }
+
+  off(event, callback) {
+    if (this.listeners[event]) {
+      this.listeners[event] = this.listeners[event].filter((listener) => listener !== callback);
+    }
+  }
+}
+
+export const eventBus = new EventBus();
