530f6aa14e8e4792e7dc64360c7404f7fe5c6bec
siamese-lang <askj95@naver.com>
2026-07-14 21:03:21 +0900

    Load Spring context with local runtime baseline

--- backend/src/test/java/com/terraformers/modernization/TerraformersBackendApplicationTest.java
+++ backend/src/test/java/com/terraformers/modernization/TerraformersBackendApplicationTest.java
@@ -1,11 +1,12 @@
 package com.terraformers.modernization;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class TerraformersBackendApplicationTest {
 
     @Test
-    void applicationClassIsLoadable() {
-        TerraformersBackendApplication application = new TerraformersBackendApplication();
+    void contextLoadsWithLocalStubRuntime() {
     }
 }
