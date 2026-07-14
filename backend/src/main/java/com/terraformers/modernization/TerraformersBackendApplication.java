cd55593cb17a3c55a73379a91817dcc5ec329e42
siamese-lang <askj95@naver.com>
2026-07-14 19:59:44 +0900

    Enable analysis runtime configuration

--- backend/src/main/java/com/terraformers/modernization/TerraformersBackendApplication.java
+++ backend/src/main/java/com/terraformers/modernization/TerraformersBackendApplication.java
@@ -1,12 +1,13 @@
 package com.terraformers.modernization;
 
+import com.terraformers.modernization.analysis.AnalysisRuntimeProperties;
 import com.terraformers.modernization.config.RuntimeContractProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(RuntimeContractProperties.class)
+@EnableConfigurationProperties({RuntimeContractProperties.class, AnalysisRuntimeProperties.class})
 public class TerraformersBackendApplication {
 
     public static void main(String[] args) {
