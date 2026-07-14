5cb913a62ceef8abe5ed290eac63f47c05aca138
siamese-lang <askj95@naver.com>
2026-07-14 19:03:29 +0900

    Add backend application smoke test

---
+++ backend/src/test/java/com/terraformers/modernization/TerraformersBackendApplicationTest.java
@@ -0,0 +1,11 @@
+package com.terraformers.modernization;
+
+import org.junit.jupiter.api.Test;
+
+class TerraformersBackendApplicationTest {
+
+    @Test
+    void applicationClassIsLoadable() {
+        TerraformersBackendApplication application = new TerraformersBackendApplication();
+    }
+}
