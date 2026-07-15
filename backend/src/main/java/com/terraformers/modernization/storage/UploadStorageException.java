1a91f478676cc0383019dfb7ed1c99548ee83a08
siamese-lang <askj95@naver.com>
2026-07-15 10:30:16 +0900

    Add upload storage exception

---
+++ backend/src/main/java/com/terraformers/modernization/storage/UploadStorageException.java
@@ -0,0 +1,8 @@
+package com.terraformers.modernization.storage;
+
+public class UploadStorageException extends RuntimeException {
+
+    public UploadStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
