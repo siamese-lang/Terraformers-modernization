1b3dce1034db4af3ed8c4fe42c66394ce628a09c
siamese-lang <askj95@naver.com>
2026-07-20 23:52:05 +0900

    Finalize AOSS VPC endpoint permissions

--- infra/terraform/bootstrap/aws-live-foundation/rag-runtime-apply-permissions.tf
+++ infra/terraform/bootstrap/aws-live-foundation/rag-runtime-apply-permissions.tf
@@ -4,6 +4,16 @@ data "aws_iam_policy_document" "terraform_apply_rag_runtime_create_effective" {
   ]
 
   statement {
+    sid       = "CreateAossManagedVpcEndpointWithRegionalDependencies"
+    effect    = "Allow"
+    actions   = ["ec2:CreateVpcEndpoint"]
+    resources = [
+      "arn:aws:ec2:ap-northeast-2:${var.expected_aws_account_id}:subnet/*",
+      "arn:aws:ec2:ap-northeast-2:${var.expected_aws_account_id}:security-group/*",
+    ]
+  }
+
+  statement {
     sid       = "RevokeTaggedRagSecurityGroupIngress"
     effect    = "Allow"
     actions   = ["ec2:RevokeSecurityGroupIngress"]
